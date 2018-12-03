a8a759a90d5e540601d60ab872a2c6391dc21fab
vijayakrishnajava <vijayakrishnajava@gmail.com>
2018-12-03 12:12:17 -0800

    changed base package structure

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package io.agileintelligence.ppmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
