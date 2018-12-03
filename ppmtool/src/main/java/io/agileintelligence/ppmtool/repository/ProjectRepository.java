659dda5cd97482b9de6c33b6bbaf45b5261fc005
vijayakrishnajava <vijayakrishnajava@gmail.com>
2018-12-03 13:22:14 -0800

    added repository

---
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.agileintelligence.ppmtool.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import io.agileintelligence.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	
+}
